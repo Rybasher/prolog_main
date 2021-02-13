70c8bdd507a1b6e0e7b5ed4ce80baac7ecb5602d
Rybasher <kyrary99@gmail.com>
2021-02-13 11:10:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	effect: "slide",
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+	},
+
+	// Navigation arrows
+
+
+	// And if we need scrollbar
+
+});
